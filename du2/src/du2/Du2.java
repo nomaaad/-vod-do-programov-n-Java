ce46e4a65533e377ae4482a6e578031e84293122
nomaaad <33545500+nomaaad@users.noreply.github.com>
2017-12-07 20:02:50 +0100

    idw v 1 bodu

--- du2/src/du2/Du2.java
+++ du2/src/du2/Du2.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.DoubleStream;
 
 /**
  * IDW
@@ -24,9 +25,9 @@ public class Du2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double []x = new double[21];
-        double []y = new double[21];
-        double []z = new double[21];
+        double []x = new double[20];
+        double []y = new double[20];
+        double []z = new double[20];
         try {
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
             String line;
@@ -41,7 +42,7 @@ public class Du2 {
                 //    continue;
                 //}
                 for (int i=0; i<items.length; i++){
-                    System.out.println(items[i]);
+                    //System.out.println(items[i]);
                     Double.parseDouble(items[i]);
                     if (i==0){
                         x[j]=(Double.parseDouble(items[i]));
@@ -62,15 +63,19 @@ public class Du2 {
             System.err.print("Error while reading a line");
             System.exit(1);
         }
+        
+        //System.out.println(IDW1b(x, y, z, 200, 220, 2));
+        System.out.println(IDW1b(x, y, z, 200, 220, 2));
+        
         PrintWriter writer;
         try {
             writer = new PrintWriter(args[1]);
-            for(int i=0; i<20; i++){
-                if(i%99==0){
-                    writer.println(z[i]);
+            for(int i=1; i<20; i++){
+                if(i%100==0){
+                    writer.println(z[i-1]);
                 }
                 else{    
-                    writer.print(z[i]+",");
+                    writer.print(z[i-1]+",");
                 }
             }
             writer.close();
@@ -79,6 +84,43 @@ public class Du2 {
         }
     }
     
+    public static double IDW1b(double []xd, double []yd, double []zd, double x, double y, double al){
+    /** 
+     * 
+     * 
+     * @param   lat     pole zemepisnych sirek
+     * @param   lon     pole zemepisnych delek
+     * @param   scale   celociselne meritkove cislo
+     * @param   R       desetinne cislo polomeru Zeme
+     */        
     
+    int nd=xd.length;
+    double r;
+    double z;
+    double []lam = new double[nd];
+    double []lambda = new double[nd];
+    double []zi = new double[nd];
     
+    for (int i=1; i<nd; i++){
+        r=Math.sqrt(Math.pow(x-xd[i],2)+Math.pow(y-yd[i],2));
+        if (r==0){
+            return zd[i];
+        }
+        else{
+            lam[i]=1/Math.pow(r,al);
+        }
+    }
+    
+    //
+    double lamSum = DoubleStream.of(lam).sum();
+    for (int i=0; i<lam.length; i++){
+        lambda[i]=lam[i]/lamSum;
+    }
+    
+    //
+    for (int i=0; i<lam.length; i++){
+        zi[i]=zd[i]*lambda[i];
+    }
+    return DoubleStream.of(zi).sum();
+    } 
 }
