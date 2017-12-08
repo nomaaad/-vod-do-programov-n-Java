e20cd616850add7b7ad737b12e2f01f027356a53
nomaaad <33545500+nomaaad@users.noreply.github.com>
2017-12-08 23:06:23 +0100

    volitelne rozliseni a exponent

--- du2/src/du2/Du2.java
+++ du2/src/du2/Du2.java
@@ -27,13 +27,31 @@ public class Du2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int res = 100;
+        int resX = 100;
+        int resY = 100;
         double alfa = 2;
-        
+        try{
+            String [] XxY = args[3].split("x");
+            resX = Integer.parseInt(XxY[0]);
+            resY = Integer.parseInt(XxY[1]);
+            alfa = Double.parseDouble(args[2]);
+        } catch(NumberFormatException ex){
+            System.err.print("Incorrect format of argument");
+            System.exit(1);
+        } 
+            
         String tFile = args[0];
         String []fileArr = loadT(tFile);
-                
-        int n = Integer.parseInt(fileArr[0]);
+        
+        int n = 20;
+        try{        
+            n = Integer.parseInt(fileArr[0]);
+        } catch(NumberFormatException ex){
+            System.err.print("First line must state number of following lines");
+            System.exit(1);
+        }
+        
+//        System.out.println("n: "+n);
                 
         double []xd = new double[n];
         double []yd = new double[n];
@@ -47,39 +65,39 @@ public class Du2 {
                 for (int i=0; i<items.length; i++){
                     Double.parseDouble(items[i]);
                     if (items.length!=3){
-                        System.err.print("False number of value(s) in line");
+                        System.err.print("Incorrect number of value(s) in line");
                         System.exit(1);
                     }
                     if (i==0){
                         xd[j-1]=(Double.parseDouble(items[i]));
-                        System.out.println("x: "+items[i]);
+//                        System.out.println("x: "+items[i]);
                     }
                     if (i==1){
                         yd[j-1]=(Double.parseDouble(items[i]));
-                        System.out.println("y: "+items[i]);
+//                        System.out.println("y: "+items[i]);
                     }
                     if (i==2){
                         zd[j-1]=(Double.parseDouble(items[i]));
-                        System.out.println("z: "+items[i]);
+//                        System.out.println("z: "+items[i]);
                     }
                 }
             }
         } catch(ArrayIndexOutOfBoundsException ex){
-            System.err.print("Missing line(s) in file");
+            System.err.print("Missing line(s) in input file");
             System.exit(1);
         } catch(NumberFormatException ex){
-            System.err.print("NaN in file");
+            System.err.print("NaN in input file");
             System.exit(1);
         }
         
-        double []xx = getGrid(xd, res);
-        double []yy = getGrid(yd, res);
+        double []xx = getGrid(xd, resX);
+        double []yy = getGrid(yd, resY);
         
         PrintWriter writer;
         try {
             writer = new PrintWriter(args[1]);
-            for(int i=0; i<res; i++){
-                for(int j=0; j<res; j++){
+            for(int j=0; j<resY; j++){
+                for(int i=0; i<resX; i++){
                     writer.print(Math.round(IDW1p(xd, yd, zd, xx[i], yy[j], alfa)*100)/100.0+",");
                 }
                 writer.println();
